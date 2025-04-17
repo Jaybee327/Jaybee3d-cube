2d41abdb537de52c62dd3d5b77acc10270a209c6
Jaybee327 <164274598+Jaybee327@users.noreply.github.com>
2025-04-17 03:13:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ let camera = {
 
 let dragging = false;
 let previousMousePosition = null;
+let previousTouchPosition = null;
 
+const rotationSpeed = isMobile() ? 0.1 : 0.5;
+
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+// Mouse event listeners
 document.addEventListener('mousedown', (event) => {
   dragging = true;
   previousMousePosition = [event.clientX, event.clientY];
@@ -20,8 +28,8 @@ document.addEventListener('mousemove', (event) => {
       currentMousePosition[1] - previousMousePosition[1]
     ];
 
-    camera.rotation[0] -= deltaMousePosition[1] * 0.5;
-    camera.rotation[1] += deltaMousePosition[0] * 0.5;
+    camera.rotation[0] -= deltaMousePosition[1] * rotationSpeed;
+    camera.rotation[1] += deltaMousePosition[0] * rotationSpeed;
 
     previousMousePosition = currentMousePosition;
   }
@@ -31,6 +39,33 @@ document.addEventListener('mouseup', () => {
   dragging = false;
 });
 
+// Touch event listeners
+document.addEventListener('touchstart', (event) => {
+  dragging = true;
+  previousTouchPosition = [event.touches[0].clientX, event.touches[0].clientY];
+});
+
+document.addEventListener('touchmove', (event) => {
+  event.preventDefault();
+  if (dragging) {
+    const currentTouchPosition = [event.touches[0].clientX, event.touches[0].clientY];
+    const deltaTouchPosition = [
+      currentTouchPosition[0] - previousTouchPosition[0],
+      currentTouchPosition[1] - previousTouchPosition[1]
+    ];
+
+    camera.rotation[0] -= deltaTouchPosition[1] * rotationSpeed;
+    camera.rotation[1] += deltaTouchPosition[0] * rotationSpeed;
+
+    previousTouchPosition = currentTouchPosition;
+  }
+});
+
+document.addEventListener('touchend', () => {
+  dragging = false;
+});
+
+// Update cube's transform style based on camera's rotation
 function updateCubeTransform() {
   const rotateX = camera.rotation[0];
   const rotateY = camera.rotation[1];
@@ -38,6 +73,7 @@ function updateCubeTransform() {
   cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 }
 
+// Animate and update cube's transform
 function animate() {
   requestAnimationFrame(animate);
   updateCubeTransform();
